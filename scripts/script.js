7b3f834fd81168d23cc3ec7202294c771310cbc5
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-01-23 01:56:22 +0300

    Fixed header, added mode-switcher js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   $('.header__burger').click(function(event) {
-    $('.header__burger,.header__burger-middle,.header__menu').toggleClass('active');
+    $('.header__burger,.header__burger-middle,.header__menu,.mode-switcher').toggleClass('active');
     $('.page').toggleClass('lock'); //I block page scroll while the menu is open step 15 css
   });
 });
 //toggle -- You click and class 'active' is added,
 //you click again and the class is deleted
+
+$(document).ready(function() {
+$('.mode-switcher__switch-btn').click(function(){
+  $(this).toggleClass('switch-dark');
+  $('.mode-switcher__light-pic,.mode-switcher__dark-pic').toggleClass('active');
+});
+});
