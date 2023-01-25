6ab67c5c0673ae75d3f75e79baf3d21f968f284d
Anastasiia Kostina <Kaliarmon@yandex.ru>
2023-01-25 13:17:36 +0300

    Slightly changed js lead, added intro-quote

--- scripts/script.js
+++ scripts/script.js
@@ -15,9 +15,18 @@ $('.mode-switcher__switch-btn').click(function(){
 });
 
 //-----lead---- background when sticky
+// https://translated.turbopages.org/proxy_u/en-ru.ru.77646c9b-63d062ea-dcd90422-74722d776562/https/stackoverflow.com/questions/17900892/how-to-make-background-colour-fade-in-when-div-is-scrolled-down
 $(document).scroll(function(){
   t = (30 - $(this).scrollTop())/100;
   if(t<0)t=0;
+  $('.lead__sticky-cover_top').css({opacity: t});
   $('.lead__sticky-cover_bottom').css({opacity: t});
-  $('.lead__sticky-cover_top').css({opacity: t})
-})
+});
+
+//this doesn't help much but mb I screwed the function https://translated.turbopages.org/proxy_u/en-ru.ru.1bbd32c7-63d0ea2d-24bb8462-74722d776562/https/stackoverflow.com/questions/4188903/opposite-of-scrolltop-in-jquery
+$.fn.scrollBottom = (function() {
+  return $(document).height() - this.scrollTop() - this.height();
+
+});
+$(window).scrollBottom(300);
+
